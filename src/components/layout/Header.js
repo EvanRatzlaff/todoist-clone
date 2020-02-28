0a3be9b3b9542ce986a4441fe8ef3b6cf98700b4
Evan_Ratzlaff <ratzlaff92@gmail.com>
2020-02-28 12:42:15 -0500

    3rd commit added react-icons

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,20 @@
 import { React } from 'react';
+import { FaAngellist } from 'react-icons/fa'
 
 export const Header = () => {
     return (
-        <p>I'm a header</p>
+        <header className="header" data-testid="header">
+            <nav>
+                <div className="logo">
+                    <img src="/iages/logo.png" alt="Todoist" />
+                </div>
+                <div className="settings">
+                    <ul>
+                        <li>*</li>
+                        <li><FaAngellist /></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
     )
 }
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+
+export const Sidebar = () => {
+    <p>sidebar here!</p>
+}
